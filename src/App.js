4e0e4870f5cf71cc43aa1ed7c60107cbadba55d4
AustenYee <austenyee@gmail.com>
2023-02-23 00:17:45 -0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+
+import React from 'react';
+import Navbar from './Navbar';
 import './App.css';
-import { Routes, Route, BrowserRouter} from 'react-router-dom';
+// import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import JaneHopkinsDoctor from './pages/JaneHopkinsDoctor';
 import JaneHopkinsAdmin from './pages/JaneHopkinsAdmin';
@@ -8,14 +12,26 @@ import FDA from './pages/FDA';
 
 function App() {
   return (
-  <Routes>
-    <Route exact path="/" element={<Login/>}> </Route>
-    <Route path="/JaneHopkins/doctor" element= { <JaneHopkinsDoctor/> }> </Route>
-    <Route path="/JaneHopkins/admin" element= { <JaneHopkinsAdmin/> }> </Route>
-    <Route path="/Bavaria" element={<Bavaria />} />
-    <Route path="/FDA" element={<FDA />} />
-  </Routes>
-  )
+    <>
+
+        <Navbar></Navbar>
+        <Routes>
+          {/* <Route path='/' exact/>
+          <Route path='/FDA' component={FDA} />
+          <Route path='/Bavaria' component={Bavaria} />
+          <Route path='/JaneHopkinsDoctor' component={JaneHopkinsDoctor} />
+          <Route path='/JaneHopkinsAdmin' component={JaneHopkinsAdmin} />
+          <Route path='/Login' component={Login} /> */}
+          <Route path='/' exact/>
+          <Route path='/FDA' exact element={<FDA/>} />
+          <Route path='/Bavaria' exact element={<Bavaria/>} />
+          <Route path='/JaneHopkinsDoctor' exact element={<JaneHopkinsDoctor/>} />
+          <Route path='/JaneHopkinsAdmin' exact element={<JaneHopkinsAdmin/>} />
+          <Route path='/Login' exact element={<Login/>} />
+        </Routes>
+
+    </>
+  );
 }
 
 export default App;
