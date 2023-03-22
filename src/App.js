7ea688c8de5ce479fe3514a16d1511130264b327
Noel Ian Paulite <npaulite@csus.edu>
2023-03-21 17:12:37 -0700

    role-based access

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
+import { auth, db } from './firebase-config'
+import { onAuthStateChanged } from '@firebase/auth';
+import {doc, getDoc} from "firebase/firestore"
 import Navba from './Navba';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -10,27 +13,75 @@ import FDA from './pages/FDA';
 import AddForm from './pages/AddForm';
 import UpdateForm from './pages/UpdateForm';
 import SendDrugs from './pages/SendDrugs';
+import RequireAuth from './RequireAuth';
 
+export const UserContent = createContext(null);
+
+export const SignOut = () => {
+  auth.signOut()
+  window.location.reload(false)
+}
 
 function App() {
 
+  const [user, setUser] = useState()
+  const [id, setID] = useState()
+  const [userDetail, setUserDetail] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    role: ""
+  })
+
+  const getUserDetails = (id) => {
+    const usersRef = doc(db, 'users', id)
+    return getDoc(usersRef)
+  }
+
+  onAuthStateChanged(auth, (currentUser) => {
+    if(currentUser) {
+    setUser(currentUser)
+    setID(currentUser.uid)
+    if(id) {
+      getUserDetails(id)
+      .then(user => {
+        if(user.exists) {
+          setUserDetail(user.data())
+        }
+      })
+      }}}
+    )
+
+    
   return (
-    <>
+    <div className='app'>
+      <UserContent.Provider value={{user: user, setUser: setUser,
+        id: id, setID: setID, userDetail: userDetail, setUserDetail: setUserDetail
+      }}>
         <Navba></Navba>
         <div>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route element={<RequireAuth allowedRoles={['FDA', 'Admin']} />}>
           <Route path='/FDA'  element={<FDA/>} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={['Bavaria', 'Admin']} />}>
           <Route path='/Bavaria'  element={<Bavaria/>} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={['JaneHopkinsDoctor', 'Admin']} />}>
           <Route path='/JaneHopkinsDoctor'  element={<JaneHopkinsDoctor/>} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={['JaneHopkinsAdmin', 'Admin']} />}>
           <Route path='/JaneHopkinsAdmin'  element={<JaneHopkinsAdmin/>} />
+          </Route>
           <Route path='/Login'  element={<Login/>} />
           <Route path='/JaneHopkinsDoctor/AddPatient'  element={<AddForm/>} />
           <Route path='/JaneHopkinsDoctor/UpdatePatient'  element={<UpdateForm/>} />
           <Route path='/Bavaria/SendDrugs' element={<SendDrugs/>} />
         </Routes>
         </div>
-   </>
+        </UserContent.Provider>
+   </div>
   );
 }
 
