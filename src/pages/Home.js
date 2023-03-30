5664b671add7e83d86258a539723c51b2ef1dc56
Noel Ian Paulite <npaulite@csus.edu>
2023-03-29 21:24:12 -0700

    hetvi's work with some edits

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,25 +8,12 @@ import { Container } from "@mui/material";
 
     <div className= "main">
       <div className="container">
-        <div className="img">
-
-    <div className="main">
       <div className="img">
-
           <a href="https://www.vendia.com/">
           <img src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/cpuf13twbf75y9bjau7h.png"/>
           </a>
         </div>
-
       </div> 
-        <div className='myBox'>
-          <Container>
-          <h1 className='vendia'>Vendia: Bring trust to data</h1>
-          <h2 className='info_v'>Vendia Share is built to make data sharing easier 
-              between partners. To bring trust and a single source of truth to partner data sharing and secure, 
-              compliant, real-time collaboration — no matter your partners'  geographies, stack of systems, data 
-              architecture, or clouds. It combines business blockchain, smart APIs, and cloud databases.
-
       <div className="myBox">
         <Container>
           <h1 className="vendia">Vendia: Bring trust to data</h1>
